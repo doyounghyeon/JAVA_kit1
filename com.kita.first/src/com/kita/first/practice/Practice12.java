8f5e1d7ea71832218eac3a98fd64ea6c5f84af66
doyounghyeon <dusgus1104@naver.com>
2021-01-04 15:48:50 +0900

    2021.01.04

--- com.kita.first/src/com/kita/first/practice/Practice12.java
+++ com.kita.first/src/com/kita/first/practice/Practice12.java
@@ -5,12 +5,16 @@ public class Practice12 {
 		// *찍기
 		int star = 3;
 		
+		// ***
 		for(int i = 0; i<3; i++) {
 			System.out.printf("*",i);
 			
 		}
 		System.out.println();
 		
+		// *
+		// *
+		// *
 		for(int i = 0; i<3; i++) {
 			System.out.printf("*\n",i);
 			
