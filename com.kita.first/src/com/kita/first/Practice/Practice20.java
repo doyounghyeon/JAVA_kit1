aeb23f3ab811aa51030e4ade9cf409dd3cd182a8
doyounghyeon <dusgus1104@naver.com>
2021-01-07 13:59:18 +0900

    2021.01.07

--- com.kita.first/src/com/kita/first/Practice/Practice20.java
+++ com.kita.first/src/com/kita/first/Practice/Practice20.java
@@ -2,8 +2,15 @@ package com.kita.first.Practice;
 
 public class Practice20 {
 	public static void main(String[] args) {
-		// whiel문에 조건식을 사용하요
+		// while문에 조건식을 사용하요
 		// 1~100을 모두 더한 값이 출력되도록 하세요
 		// 합계 : 5050
+		 int n=0;
+		 int s = 0;
+		 while(n<100){    
+		   n++;
+		   s+=n;
+		  }
+		  System.out.println("합계 : " + s);
 	}
 }
